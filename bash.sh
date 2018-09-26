a08d079cf189a0cc71e1a85eec4bd21dc07cbe5b
fernando <fernado.shui@gmail.com>
2018-09-26 14:33:07 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-images=(kube-proxy-amd64:v1.11.1 kube-scheduler-amd64:v1.11.1 kube-controller-manager-amd64:v1.9.0 kube-apiserver-amd64:v1.11.1 
-etcd-amd64:3.1.17 pause-amd64:3.0 dashboard:v1.8.3 k8s-dns-sidecar-amd64:1.14.10 k8s-dns-kube-dns-amd64:1.14.10
+images=(kube-proxy-amd64:v1.11.1 kube-scheduler-amd64:v1.11.1 kube-controller-manager-amd64:v1.9.0 kube-apiserver-amd64:v1.11.1 etcd-amd64:3.1.17 pause-amd64:3.0 kubernetes-dashboard-amd64:v1.8.3 k8s-dns-sidecar-amd64:1.14.10 k8s-dns-kube-dns-amd64:1.14.10
 k8s-dns-dnsmasq-nanny-amd64:1.14.10)
 for image in ${images[@]} ; do
   docker pull fernandox/$image
