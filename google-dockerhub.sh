c0bc41c7587903c582b2a36cdb0c58c7e68b171a
fernado <fernado.shui@gmail.com>
2018-12-11 23:22:48 +0800

    update

--- google-dockerhub.sh
+++ google-dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-images=(etcd:3.2.24 kube-controller-manager:v1.13.0 kube-apiserver:v1.13.0 kube-scheduler:v1.13.0 kube-proxy:v1.13.0 pause-amd64:3.1 coredns:1.2.6)
+images=(etcd:3.2.24 kube-controller-manager:v1.13.0 kube-apiserver:v1.13.0 kube-scheduler:v1.13.0 kube-proxy:v1.13.0 pause-amd64:3.1 coredns:1.2.6 kubernetes-dashboard:1.10.0)
 for image in ${images[@]} ; do
   docker pull fernandox/$image
   docker tag fernandox/$image gcr.io/google_containers/$image
